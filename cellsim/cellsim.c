7abbe9a732a8be21e5a8a23e41669cfacdf21489
Previo Prakasa <previo.prakasa@gmail.com>
2024-08-24 00:19:00 +0700

    set c99, added ent placeholder

--- cellsim/cellsim.c
+++ cellsim/cellsim.c
@@ -2,5 +2,11 @@
 #include "cellsim.h"
 
 int main(void) {
+	clsm_ent* ent = (clsm_ent*)clsm_malloc(sizeof(clsm_ent));
+	clsm_ent_init(ent);
+	printf("%f\n", ent->v3pos[0]);
+	printf("%f\n", ent->v3pos[1]);
+	printf("%f\n", ent->v3pos[2]);
+	if (ent == NULL) exit(1); clsm_free(ent);
 	return 0;
 }
--- cellsim/cellsim.h
+++ cellsim/cellsim.h
@@ -1,11 +1,12 @@
-#ifndef _CLSM_H_
-#define _CLSM_H_
+#ifndef _CLSM_H
+#define _CLSM_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
 #ifdef CLSM_INCLUDES
+#include <stdlib.h>
 #include <stdio.h>
 #endif // CLSM_INCLUDES
 
@@ -27,13 +28,34 @@ do { \
 #define clsm_dbg_fatal(msg) clsm_dbg_f(stderr, "Fatal", "\033[0;31m", msg)
 #endif // NDEBUG
 
-#ifndef CLSM_MALLOC
+#ifndef clsm_malloc
 #define clsm_malloc(size) malloc(size)
-#define clsm_free(size) free(size)
-#endif // CLSM_MALLOC
+#endif // clsm_malloc
+
+#ifndef clsm_free
+#define clsm_free(p) free(p)
+#endif // clsm_free
+
+#define vec2_init(T) (T*)calloc(2, sizeof(T))
+#define vec3_init(T) (T*)calloc(3, sizeof(T))
+
+typedef struct {
+	int id;
+	double* v3pos;
+	double* v3vel;
+	double* v3acc;
+	double* v2hea;
+} clsm_ent;
+
+void clsm_ent_init(clsm_ent* ent) {
+	ent->v3pos = vec3_init(double);
+	ent->v3vel = vec3_init(double);
+	ent->v3acc = vec3_init(double);
+	ent->v2hea = vec2_init(double);
+}
 
 #ifdef __cplusplus
 extern }
 #endif // __cplusplus
 
-#endif // _CLSM_H_
+#endif // _CLSM_H
